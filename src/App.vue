095998f5ff536d8718157c10cb3ad34455c86791
Thomas Milton <perso.milton@gmail.com>
2022-02-17 17:40:45 +0100

    Modification des fichiers

--- src/App.vue
+++ src/App.vue
@@ -51,4 +51,57 @@ h2{
   background: url('./assets/network.jpg');
   height: auto;
 }
+
+@media screen and (max-width: 640px) {
+  #team-infos {
+    display: flex;
+    flex-direction: column;
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  .title-font {
+    color: rgb(193, 162, 109);
+    text-align: center;
+    font-size: 30px;
+  }
+
+  #team img {
+    height: 60px;
+    width: 60px;
+    border-radius: 90%;
+    border: 3px solid rgb(193, 162, 109);
+    filter: grayscale(1);
+  }
+
+  #team img:hover {
+    filter: grayscale(0);
+  }
+
+  .presentation {
+    margin: 0;
+  }
+
+  .presentation p {
+    margin: 0 2%;
+  }
+
+  #localisation {
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 30px;
+  }
+
+  #adress {
+    margin: auto;
+    padding-bottom: 10px;
+  }
+
+  .map-configuration {
+    width: auto;
+    height: auto;
+  }
+}
 </style>
